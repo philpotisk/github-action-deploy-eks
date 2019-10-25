9fcf4f4cddffbf484666fc1100dfd24a235ef560
Philipp Potisk <philipp.potisk@danubetech.com>
2019-10-25 17:22:46 +0200

    Added namespace

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,16 +11,16 @@ chmod 400 ./id_rsa
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 ssh-agent bash -c 'ssh-add ./id_rsa; git clone git@ec2-13-59-213-19.us-east-2.compute.amazonaws.com:/home/git/uniresolver-kubernetes'
 
+cd ./uniresolver-kubernetes
 ls -al
 
 
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
-IS_DEPLOYED=$(kubectl get deployments |grep ${CONTAINER_NAME})
+IS_DEPLOYED=$(kubectl -n uni-resolver get deployments |grep ${CONTAINER_NAME})
 
-cd ./uniresolver-kubernetes
-ls -al
+
 
 #echo "Creating cluster (deployment-${CONTAINER_NAME}.yml) ..."
 #kubectl create -f deployment-${CONTAINER_NAME}.yml
@@ -30,11 +30,11 @@ ls -al
 if [ -z "${IS_DEPLOYED}" ]
 then
    echo "Creating cluster (${CONTAINER_NAME}-deployment.yaml) ..."
-   kubectl create -f ${CONTAINER_NAME}-deployment.yaml
-   kubectl create -f ${CONTAINER_NAME}-service.yaml
+   kubectl n uni-resolver create -f ${CONTAINER_NAME}-deployment.yaml
+   kubectl n uni-resolver create -f ${CONTAINER_NAME}-service.yaml
 else
    echo "Updating cluster ..."
-   kubectl rollout restart -f ${CONTAINER_NAME}-deployment.yaml
+   kubectl n uni-resolver rollout restart -f ${CONTAINER_NAME}-deployment.yaml
 fi
 
-kubectl rollout restart -f uni-resolver-ingress.yaml
+kubectl n uni-resolver rollout restart -f uni-resolver-ingress.yaml
