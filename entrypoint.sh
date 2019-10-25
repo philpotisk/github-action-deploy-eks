8d47f5ae4fa0412fbc5df42e0b622a26665ee721
Philipp Potisk <philipp.potisk@danubetech.com>
2019-10-25 17:55:42 +0200

    unset of AWS credentials

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,10 +14,12 @@ ssh-agent bash -c 'ssh-add ./id_rsa; git clone git@ec2-13-59-213-19.us-east-2.co
 cd ./uniresolver-kubernetes
 ls -al
 
-
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+kubectl version --client --short
 echo "Checking deployment state of ${CONTAINER_NAME} ..."
 IS_DEPLOYED=$(kubectl -n uni-resolver get deployments |grep ${CONTAINER_NAME})
 
