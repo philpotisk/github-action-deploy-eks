71b54103c458b6944475d27d5cd2c1b48fa888d0
Philipp Potisk <philipp.potisk@danubetech.com>
2019-10-25 16:29:51 +0200

    Adjusted file paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ ls -al
 echo "$EXTERNAL_GIT_KEY" | base64 --decode > ./id_rsa
 chmod 400 ./id_rsa
 export GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-ssh-agent bash -c 'ssh-add ./id_rsa; git clone git@ec2-13-59-213-19.us-east-2.compute.amazonaws.com:/home/git/repo'
+ssh-agent bash -c 'ssh-add ./id_rsa; git clone git@ec2-13-59-213-19.us-east-2.compute.amazonaws.com:/home/git/uniresolver-kubernetes'
 
 ls -al
 
@@ -19,8 +19,7 @@ export KUBECONFIG=/tmp/config
 
 IS_DEPLOYED=$(kubectl get deployments |grep ${CONTAINER_NAME})
 
-echo ${CONTAINER_PATH}
-cd ${CONTAINER_PATH}
+cd ./uniresolver-kubernetes
 ls -al
 
 #echo "Creating cluster (deployment-${CONTAINER_NAME}.yml) ..."
@@ -30,10 +29,12 @@ ls -al
 
 if [ -z "${IS_DEPLOYED}" ]
 then
-   echo "Creating cluster (deployment-${CONTAINER_NAME}.yml) ..."
-   kubectl create -f deployment-${CONTAINER_NAME}.yml
-   kubectl create -f nodeport-${CONTAINER_NAME}.yml
+   echo "Creating cluster (${CONTAINER_NAME}-deployment.yaml) ..."
+   kubectl create -f ${CONTAINER_NAME}-deployment.yaml
+   kubectl create -f ${CONTAINER_NAME}-service.yaml
 else
    echo "Updating cluster ..."
-   kubectl rollout restart -f deployment-${CONTAINER_NAME}.yml
+   kubectl rollout restart -f ${CONTAINER_NAME}-deployment.yaml
 fi
+
+kubectl rollout restart -f uni-resolver-ingress.yaml
