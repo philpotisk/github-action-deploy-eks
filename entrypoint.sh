11225e0edd2815bddb6db50bdc2abe96dd560c03
Philipp Potisk <philipp.potisk@danubetech.com>
2019-10-25 11:51:31 +0200

    Added git clone via ssh

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,10 @@ cd ${CONTAINER_PATH}
 
 ls -al
 
+echo "$EXTERNAL_GIT_KEY" | base64 --decode > ./id_rsa
+ssh-agent bash -c 'ssh-add ./id_rsa; git clone git@ec2-13-59-213-19.us-east-2.compute.amazonaws.com:/home/git/repo'
+
+ls -al
 #echo "Creating cluster (deployment-${CONTAINER_NAME}.yml) ..."
 #kubectl create -f deployment-${CONTAINER_NAME}.yml
 #kubectl create -f nodeport-${CONTAINER_NAME}.yml
