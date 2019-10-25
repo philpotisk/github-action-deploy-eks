00d8506f7be6c8fe87f0da9fe53e3d12cc3a2d7b
Philipp Potisk <philipp.potisk@danubetech.com>
2019-10-25 12:12:40 +0200

    Test git-clone external

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,14 @@ echo "EKS Deloyment of ${CONTAINER_NAME}"
 
 set -e
 
+ls -al
+
+echo "$EXTERNAL_GIT_KEY" | base64 --decode > ./id_rsa
+ssh-agent bash -c 'ssh-add ./id_rsa; git clone git@ec2-13-59-213-19.us-east-2.compute.amazonaws.com:/home/git/repo'
+
+ls -al
+
+
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
@@ -11,13 +19,8 @@ IS_DEPLOYED=$(kubectl get deployments |grep ${CONTAINER_NAME})
 
 echo ${CONTAINER_PATH}
 cd ${CONTAINER_PATH}
-
 ls -al
 
-echo "$EXTERNAL_GIT_KEY" | base64 --decode > ./id_rsa
-ssh-agent bash -c 'ssh-add ./id_rsa; git clone git@ec2-13-59-213-19.us-east-2.compute.amazonaws.com:/home/git/repo'
-
-ls -al
 #echo "Creating cluster (deployment-${CONTAINER_NAME}.yml) ..."
 #kubectl create -f deployment-${CONTAINER_NAME}.yml
 #kubectl create -f nodeport-${CONTAINER_NAME}.yml
